057bcdfdbc31e0f5b622c04a760855b797781b16
acouprie <acouprie@hotmail.fr>
2020-03-07 21:59:10 +0100

    add lab41

--- ArchitectureSystemsNetworks/Architecture/lab4/lab41/lab41.c
+++ ArchitectureSystemsNetworks/Architecture/lab4/lab41/lab41.c
@@ -6,7 +6,8 @@
 #define PERIOD 100
 
 int period_pulse = MCK * PERIOD / 1000 / 8;
-float period_duration = 0.5;
+int press1 = 0, press2 = 0;
+float period_duration = 1.0f;
 
 /**
  * Synchronously write a character to US0.
@@ -26,7 +27,7 @@ void us0_puts(char *s) {
 		us0_putc(*s++);
 }
 
-/**
+/*
  * Initialize US0 (57,500 bauds, no parity, 8-bit character, 1 stop bit).
  */
 void us0_init(void) {
@@ -40,55 +41,39 @@ void us0_init(void) {
 	US0_TTGR = 10;
 }
 
-void set_cdty(float duration) {
-  PWM_CDTY(LED) = period_pulse * duration;
-}
-
-void __attribute__ ((interrupt("IRQ"))) handle_pio(void) {
-	int v;
-
-	if((PIO_PDSR & PUSH1) == 0) {
-    us0_puts("flag");
-    period_duration += 0.1;
-    set_cdty(period_duration);
-	}
-
-	v = PIO_ISR;
-	v = AIC_EOICR;
-}
-
 int main(void) {
   us0_init();
-
-  // Set the clock
-  TC0_CCR = TC_CLKDIS;
-  TC0_CMR = TC_TCCLKS_CLOCK3 | TC_CPCTRG;
-  TC0_RC = DELAY_IN_PULSES;
-  TC0_CCR = TC_SWTRG | TC_CLKEN;
 
 	/* initialization */
   // PWM
   PWM_CMR(LED) = PWM_CPRE_MCK_8 | PWM_CPOL;
   PWM_CPRD(LED) = period_pulse;
-  set_cdty(period_duration);
+  PWM_CDTY(LED) = period_pulse * period_duration;
   PWM_ENA = 1 << LED;
 
-  // interrupt button
-  AIC_IDCR = 1 << ID_PIO;
-  void handle_pio(void);
-  AIC_SVR[ID_PIO] = (uint32_t)handle_pio;
-  AIC_SMR[ID_PIO] = 0;
-
+  PIO_PER = PUSH1 | PUSH2;
   PIO_ODR = PUSH1 | PUSH2;
   PIO_IER = PUSH1 | PUSH2;
 
-  int x = AIC_EOICR;
-  AIC_ICCR = 1 << ID_PIO;
-  AIC_IECR = 1 << ID_PIO;
-  UNMASK_IRQ;
-
   /* main loop */
 	while(1) {
+    if(press1 && ((PIO_PDSR & PUSH1) == 0)) {
+      press1 = !press1;
+      if(period_duration < 1)
+        period_duration += 0.1f;
+      PWM_CDTY(LED) = period_pulse * period_duration;
+  	} else if (!press1 && (PIO_PDSR & PUSH1) != 0) {
+      press1 = !press1;
+    }
+
+    if(press2 && ((PIO_PDSR & PUSH2) == 0)) {
+      press2 = !press2;
+      if (period_duration > 0)
+        period_duration -= 0.1f;
+      PWM_CDTY(LED) = period_pulse * period_duration;
+  	} else if (!press2 && (PIO_PDSR & PUSH2) != 0) {
+      press2 = !press2;
+    }
 	}
 
 	return 0;
