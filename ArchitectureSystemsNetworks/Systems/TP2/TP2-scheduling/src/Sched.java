a46a0775ae8476a097ba91d3ce7649c14aef6ee3
acouprie <acouprie@hotmail.fr>
2020-03-30 16:01:33 +0200

    finish fifo

--- ArchitectureSystemsNetworks/Systems/TP2/TP2-scheduling/src/Sched.java
+++ ArchitectureSystemsNetworks/Systems/TP2/TP2-scheduling/src/Sched.java
@@ -16,57 +16,65 @@ class Sched extends Thread {
 				n = 0;
 			}
 
-			for (int process = 0; process < Main.nbProcess; process++) {
-				if(i % Main.processes[process].periodicity == 0) {
-					Main.inQueue[process] = true;
-				}
-			}
+			// update the periodicity queue
+			click(i);
 
 			// process election
 			Main.process = n % Main.nbProcess;
 			Process currentProcess = Main.processes[Main.process];
 
-			// FIFO implementation
-		    // if process not finished, call it again
-			Boolean gap = true;
 			int duration = 0;
+			// if elected process is ready to execute
             if (Main.inQueue[currentProcess.id] == true) {
 				duration = fifo(currentProcess);
 			} else {
+				// else we look for the next process ready
             	for (int process = 0; process < Main.nbProcess; process++) {
             	    if(Main.inQueue[process] == true) {
-            	    	gap = false;
 						duration = fifo(Main.processes[process]);
 						break;
 					} else {
-            	    	gap = true;
+            	        // if no process ready is found
+						System.out.println(i + " -");
 					}
 				}
-            	if (gap) {
-					System.out.println(i + " -");
-				}
+			}
+            // resynchronise the main loop if process executed more than one time
+            if(duration != 0) {
+				i += duration - 1;
 			}
             n++;
 			Main.lock.unlock();
-			if (duration != 0) {
-				i += duration - 1;
-			}
 		}
 
 		// print violations and timesteps used
 		System.out.print("violation: " + violations + " timesteps: " + timesteps);
     }
 
-     private int fifo(Process currentProcess) {
-		int duration = 0;
+	// FIFO implementation
+	// if process not finished, call it again
+	private int fifo(Process currentProcess) {
+		int duration;
 		 for (duration = 0; duration < currentProcess.duration; duration++) {
-			 System.out.println(i + duration + " " + currentProcess.id);
+		 	int timestep = i + duration;
+			 System.out.println(timestep + " " + currentProcess.id);
+			 // update the periodicity queue
+			 click(timestep);
 			 Main.sched = false;
 			 Main.conds[Main.process].signal();
 			 timesteps++;
 		 }
 		 Main.inQueue[currentProcess.id] = false;
 		 return duration;
+	 }
+
+	 private void click(int timestep) {
+		 for (int process = 0; process < Main.nbProcess; process++) {
+			 if (timestep % Main.processes[process].periodicity == 0) {
+				 Main.inQueue[process] = true;
+			 }
+
+		 }
 	 }
 }
 		
